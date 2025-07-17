8748116052f008f328a33bdc942910d920d9e114
Darshan Subedi <subedidarshan02@gmail.com>
2025-07-17 14:56:34 +0545

    ui complete

--- src/component/Navbar.tsx
+++ src/component/Navbar.tsx
@@ -4,8 +4,8 @@ import React from "react";
 
 function Navbar() {
     return (
-        <nav className="w-full py-4">
-            <div className="container mx-auto border-4 border-[#FF8383] rounded-4xl py-4 px-4 sm:px-8 lg:px-36 flex items-center justify-between">
+        <nav className="w-full py-4 sticky top-0 dark:bg-[#171717] bg-[#ffffff]">
+            <div className="container mx-auto border-4 border-[#FF8383] rounded-4xl py-4 px-4 sm:px-8 lg:px-36 flex items-center justify-between ">
                 <Link href={"/"}>
                     <span className="text-2xl sm:text-3xl lg:text-4xl font-bold">Dropin</span>
                 </Link>
